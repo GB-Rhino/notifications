bf986183543ff4cbf267b068bc1aa43a5a8e55d1
unknown <rygriffiths@hotmail.co.uk>
2021-05-18 21:53:06 +0100

    first draft

---
+++ Notifications.py
@@ -0,0 +1,26 @@
+from pynotifier import Notification
+import schedule
+import time
+
+
+def lmao():  ##function for the notification##
+    Notification(
+        title='test',
+        description='test',
+        duration=5,  # Duration in seconds
+        urgency='normal'
+    ).send()
+
+
+##def job():
+    ##print("I am working")
+
+
+schedule.every(10).seconds.do(lmao)
+##Requires function to be passed after do##
+##
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+##actually runs the script constant, while loop, sleeps program to prevent loop from imploding, schedule.run_pending runs given script##
